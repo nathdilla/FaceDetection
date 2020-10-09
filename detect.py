4a7918b3fa54dd6dd5f555291b943c6b02596362
Timothy Wilks <91078140@mvusd.net>
2020-10-09 10:58:17 -0700

    All faces detected

--- detect.py
+++ detect.py
@@ -6,12 +6,13 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture
 
 while True:
+   # ret, img = cap.read()
     img = cv2.imread('teens.jpg')
-   # _, img = cap.read()
+
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, 1.4, 4)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
@@ -24,3 +25,4 @@ while True:
         break
 
 cap.release()
+cv2.destroyAllWindows()
