01a7344fecdbd9751687e61afa13fbe04d47df6d
Nathan Dilla <natedillamc@gmail.com>
2020-10-11 12:09:23 -0700

    working classroomID verification

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ import mysql.connector
 class Database:
     meetingID = None
     mydb = None
-    sqlFormula = "INSERT INTO users (username, classroomID, isActive, isTeacher) VALUES (%s, %s, %s, %s)"
+    sqlFormula = "INSERT INTO users (username, classroomID, isActive, isTeacher) VALUES (%s, %s, %s, %s)" # user creation format
 
     def __init__(self):
         Database.mydb = mysql.connector.connect(
@@ -24,10 +24,18 @@ class Database:
         if isteacher == 1:
             newuser = (username, code, 0, 1)
         else:
-            newuser = (username, code, 0, 0) #check for existing code
+            sql = "SELECT * FROM users WHERE classroomID = %s"  # select all columns from users if classroomID equals code param
+            mycursor.execute(sql, (code,))  # executes sql
+            myresult = mycursor.fetchall()  # fetches results from previous execution. returns as table
+
+            size = len(myresult)  # gets size of result
+            if size > 0:  # if results are more than 0, then classroom must exists, if not, return false
+                print("classroom exists!")
+                newuser = (username, code, 0, 0)
+            else:
+                print("classroom does not exist!")
+                return False
 
         mycursor.execute(Database.sqlFormula, newuser)
         Database.mydb.commit()
-
-
-
+        return True
--- main.py
+++ main.py
@@ -26,7 +26,6 @@ def begin():
         print("Welcome, student")
         is_teacher = False
 
-
     username = raw_input("Please enter your username: ")
     password = raw_input("Please enter your password: ")
 
@@ -34,8 +33,18 @@ def begin():
     if not is_teacher:
         classroomId = raw_input("Please enter the classroom code: ")
         connection = Database()
-        connection.create_new_userconnection(username, classroomId, 0)
-        face_detect = Detect(username).scan_image() #run this loop
+        isvalid = connection.create_new_userconnection(username, classroomId, 0)
+
+        if not isvalid:
+            print("invalid response")
+            while not isvalid: # if entered classroomID is not valid keep asking for the correct one
+                classroomId = raw_input("Please enter the classroom code: ")
+                connection = Database()
+                isvalid = connection.create_new_userconnection(username, classroomId, 0)
+                if isvalid:
+                    break
+
+        #face_detect = Detect(username).scan_image() #run this loop
         ##then sendDataToTeachers()
     else:
         classroomId = raw_input("Please enter a classroom code: ")
