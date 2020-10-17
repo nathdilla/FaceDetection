a97912187d3e89c0002d40767f5b7f5a289fb3c5
Timothy Wilks <91078140@mvusd.net>
2020-10-17 15:12:57 -0700

    finished project with working face detection

--- detect.py
+++ detect.py
@@ -28,7 +28,7 @@ class Detect:
             while Detect.connected:
                 _, img = cap.read()
                 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                faces = Detect.face_cascade.detectMultiScale(gray, 1.4, 4)
+                faces = Detect.face_cascade.detectMultiScale(gray, 1.3, 5)
                 for (x, y, w, h) in faces:
                     num_of_faces = num_of_faces + 1
                     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
